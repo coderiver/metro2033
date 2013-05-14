74a6bb276bf9a17943dfcc7c81eb68b6fcde0abc
Artur Moroz <morozarturv@gmail.com>
2013-05-14 17:59:43 +0300

    +3 inner pages

--- js/common.js
+++ js/common.js
@@ -1,31 +1,41 @@
 $(document).ready(function() {
 
-	$('.mainslider__slides').cycle({
-		fx:     'scrollHorz',
-		speed:  '600',
-		timeout: 0,
-		after: onAfter,
-    		before: onBefore,
-		pager:  '.mainslider__nav',
-		prev: '.mainslider__prev',
-		next: '.mainslider__next'
-	});
-	function onBefore(currSlideElement, nextSlideElement, options, forwardFlag){
-		$('.slide__pic', currSlideElement).addClass('is-hidden').removeClass('is-animated');
-	}
-	function onAfter(currSlideElement, nextSlideElement, options, forwardFlag){
-		$('.slide__pic', nextSlideElement).removeClass('is-hidden').addClass('is-animated');
-	}
+	//slider
+	if ($('.mainslider__slides').length > 0) {
+		$('.mainslider__slides').cycle({
+			fx:     'scrollHorz',
+			speed:  '600',
+			timeout: 0,
+			after: onAfter,
+		  before: onBefore,
+			pager:  '.mainslider__nav',
+			prev: '.mainslider__prev',
+			next: '.mainslider__next'
+		});
+		function onBefore(currSlideElement, nextSlideElement, options, forwardFlag){
+			$('.slide__pic', currSlideElement).addClass('is-hidden').removeClass('is-animated');
+		}
+		function onAfter(currSlideElement, nextSlideElement, options, forwardFlag){
+			$('.slide__pic', nextSlideElement).removeClass('is-hidden').addClass('is-animated');
+		}
+	};	
 
-
-
+	//scroll
 	if ($('.news').length>0) {
 		pane = $('.news');
 		pane.jScrollPane({
 	  	autoReinitialise: true,
 	  	hideFocus: true,
 	  	animateTo: true
-	});
-	var api = pane.data('jsp');
-};
+		});
+		var api = pane.data('jsp');
+	};
+
+	//masonry
+	if ($('.js-masonry').length > 0) {
+		$('.js-masonry').masonry({
+		 	itemSelector : '.js-masonry li'
+		});
+	};
+	
 });
